86a10281dfbd971c6c496a2959d4ac24baeaac17
Oleksii Pohasii <alexeypogasiy@Alexeys-MacBook-Pro.local>
2024-02-04 12:42:04 +0100

    Start project

---
+++ src/main/java/de/telran/fixmaster/FixMasterApplication.java
@@ -0,0 +1,13 @@
+package de.telran.fixmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FixMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FixMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/telran/fixmaster/FixMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package de.telran.fixmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
