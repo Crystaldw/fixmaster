adc8cce1fbd980d9ebf9a7c7b902e2e94e657f46
Oleksii Pohasii <alexeypogasiy@Alexeys-MacBook-Pro.local>
2024-02-04 14:15:46 +0100

    created entity

---
+++ src/main/java/de/telran/fixmaster/entity/Order.java
@@ -0,0 +1,31 @@
+package de.telran.fixmaster.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+//Представляет заказ покупателя.
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date orderDate;
+    private List<OrderItem> items= new ArrayList<>();
+    private BigDecimal total = BigDecimal.ZERO;
+
+}
---
+++ src/main/java/de/telran/fixmaster/entity/Product.java
@@ -0,0 +1,31 @@
+package de.telran.fixmaster.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+//Представляет отдельный товар в магазине.
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
+    private String category;
+    private String imageURL;
+    private boolean isActive;
+
+}
