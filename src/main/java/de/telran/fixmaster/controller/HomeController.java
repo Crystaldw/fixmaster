7ac21a6dc9f24d4ce3a0df4eea5b3cbfb44f1df6
Oleksii Pohasii <alexeypogasiy@alexeys-mbp.home>
2024-02-06 10:27:59 +0100

    sort getAllProductsByCategoryId(id)

--- src/main/java/de/telran/fixmaster/controller/HomeController.java
+++ src/main/java/de/telran/fixmaster/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
@@ -24,6 +25,13 @@ public class HomeController {
     public String shop(Model model){
         model.addAttribute("categories", categoryService.getAllCategory());
         model.addAttribute("products", productService.getAllProduct());
+        return "shop";
+    }
+
+    @GetMapping("/shop/category/{id}")
+    public String shopByCategory(Model model, @PathVariable int id){
+        model.addAttribute("categories", categoryService.getAllCategory());
+        model.addAttribute("products", productService.getAllProductsByCategoryId(id));
         return "shop";
     }
 }
